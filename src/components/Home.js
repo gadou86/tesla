babba2f6237f74b0ee1e67965f424530822bb2f9
Gad Dayan <70294896+gadou86@users.noreply.github.com>
2021-06-10 15:29:03 -0400

    added styled components and props

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,34 @@ import Section from './Section'
 function Home() {
   return (
     <Container>
-      <Section />
+      <Section 
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-s.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section 
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-y.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section 
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-3.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section 
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-x.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
     </Container>
   )
 }
@@ -16,5 +43,4 @@ export default Home
 
 const Container = styled.div `
   height: 100vh;
-  border: 5px solid red;
 `
